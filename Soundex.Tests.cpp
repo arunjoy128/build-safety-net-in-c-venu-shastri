f5e883c5b6fd23696eeecea1877503bd9e985f2e
arunjoy128 <arunabrahamjoy@gmail.com>
2024-09-16 10:31:30 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -5,5 +5,4 @@ TEST(SoundexTestSuite,generateSoundexCode_ReturnsEmptyStringForEmptyStringInput)
  char soundexCode[5];
  generateSoundexCode("",soundexCode);
  EXPECT_EQ(strcmp(soundexCode,"")==0));
-
 }
