d7233445005596fd1df994c6070eb8514706b715
arunjoy128 <arunabrahamjoy@gmail.com>
2024-09-16 10:27:42 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,9 +1,9 @@
 #include <gtest/gtest.h>
-#include "Soundex.h"
 
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- //AAA
-  char soundex[5];
-  generateSoundex("AX", soundex);
-// ASSERT_EQ(soundex,"A200");
+TEST(SoundexTestSuite,generateSoundexCode_ReturnsEmptyStringForEmptyStringInput)
+{
+ char soundexCode[5];
+ generateSoundexCode("",soundexCode);
+ EXPECT_EQ(strcmp(soundexCode,"")==0));
+
 }
